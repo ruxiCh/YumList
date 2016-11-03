589573b7af8a956b546cc87be4d02c8072eafa23
ruxandra <ruxandra@syscomdigital.ro>
2016-11-03 14:31:22 +0200

    Webpack - configured loaders for sass and images

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+require("scss/style.scss");
+
 var Hello = React.createClass({
     render() {
         return (
-            <div>Heyyyyyy!!!</div>
+            <div>
+                <h1>I'm inside the component wooo LALALA!</h1>
+                <div>Heyyyyyy!!!?</div>
+                <div className="strawberry"></div>
+            </div>
         )
     }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@ path = require("path");
 var webpack = require("webpack");
 
 module.exports = {
+    resolve: {
+      root: path.resolve(__dirname),
+      modulesDirectories: ["node_modules", "src"],
+      extensions: ["", ".css", ".js", ".jsx", "sass", ".scss"]
+    },
     module: {
         loaders: [
             {
@@ -11,13 +16,31 @@ module.exports = {
                     presets: ["es2015", "react"]
                 },
                 include: path.join(__dirname, "src")
-            },
+            }, {
+                test: /\.(scss|sass)$/,
+                loader: "style-loader!css-loader!postcss-loader!sass-loader",
+                include: path.join(__dirname, "src/scss")
+            }, {
+                test: /\.(png|jpg)/,
+                loader: "url-loader?limit=10000?/!image-webpack-loader",
+                include: path.join(__dirname, "src/img")
+            }, {
+                test: /\.jsx?$/,
+                loader: "react-hot",
+                include: path.join(__dirname, "src")
+            }
         ]
     },
-    entry: ["./src/index"],
+    entry: ["webpack-dev-server/client?http://localhost:2300", "webpack/hot/only-dev-server","./src/index"],
     output: {
         path: path.join(__dirname, "dist"),
         filename: "bundle.js",
-        publicPath: "/static/"
-    }
+        publicPath: "dist/"
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+
+
 };
