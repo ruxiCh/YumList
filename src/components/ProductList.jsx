9dd28ec4dd560a56a8af2c9fce11b888b2cc2816
Ruxandra Chirea <ruxandra.chirea@siiromania.ro>
2018-02-16 17:12:42 +0200

    add_products_to_list: when a product item is clicked, its selected prop value changes

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -21,21 +21,38 @@ var ProductList = React.createClass({
 			request.send()
 		})	
 	},
+	unselectAllProducts(products) {
+		return products.map((product) => {
+			var productWithSelectedProp = Object.assign({selected: false}, product)
+			return productWithSelectedProp
+		})
+	},
 	componentWillMount() {
 		if (this.props.type === "search") {
 			this.getAllProducts().then(
 				(products) => {
+					products = this.unselectAllProducts(products)
 					this.setState({products: products})
 				}, 
 				(err) => console.error(new Error("the products cannot be loaded"))
 			)
 		}
 	},
+	selectOrUnselectProduct(product) {
+		let selectedValue = true
+		if(product.props.selected === true) {
+			selectedValue = false
+		}
+		let products = this.state.products
+		let productFromProducts = products.filter((item) =>  item.id === product.props.id)[0]
+		productFromProducts.selected = selectedValue;
+		this.setState({products: products})
+	},
 	render() {
 		return (
 			<div className='product_list'>
 				{this.state.products.map((product) => {
-					return <ProductListItem key={product.id} name={product.name} selected={false} />
+					return <ProductListItem key={product.id} id={product.id} name={product.name} selected={product.selected} selectOrUnselect={this.selectOrUnselectProduct} />
 				})}			
 			</div>
 			)
