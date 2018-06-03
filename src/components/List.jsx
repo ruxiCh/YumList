b3a485195586293e6a361a91792160558398ca5c
Ruxandra Chirea <ruxandra.chirea@gmail.com>
2018-06-03 18:09:49 +0300

    add_products_to_list: filtered the products so that only the selected products are shown in the List page. Method should be moved and tinkered with later to allow for filtering

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,6 +6,10 @@ import ProductList from "./ProductList"
 import Search from "./Search"
 
 var List = React.createClass({
+        filterProducts(products) {
+            let filteredProducts = products.filter((item) => item.selected === true)
+            return filteredProducts
+        },
         render() {
             return(
                 <div>
@@ -14,7 +18,7 @@ var List = React.createClass({
                         <GoToButton destination="Search" goToPage={this.props.goToPage} />
                     </header>
                     <div className="product_list_container">
-                        <ProductList type="list" products={this.props.products} selectOrUnselectProduct={this.props.selectOrUnselectProduct} />
+                        <ProductList type="list" products={this.filterProducts(this.props.products)} selectOrUnselectProduct={this.props.selectOrUnselectProduct} />
                     </div>
                 </div>
             )
