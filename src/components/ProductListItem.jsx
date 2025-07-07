0e3d1fbb2a2d7ac1cbdb5738045f22fdcba948a7
Ruxandra Chirea <ruxandra.chirea@Ruxandras-MacBook-Pro.local>
2025-07-07 12:39:08 +0300

    cleanup

--- src/components/ProductListItem.jsx
+++ src/components/ProductListItem.jsx
@@ -17,22 +17,4 @@ function ProductListItem({id, name, selected, toggle}) {
   );
 }
 
-class ProductListItemmm extends React.Component {
-  render() {
-    const imageSrc = `./src/img/${this.props.name.replace(" ", "_")}.jpg`;
-    return (
-      <div
-        className={`product_list_item ${this.props.selected ? "selected" : ""}`}
-        onClick={() => this.props.toggle(this)}
-      >
-        <img src={imageSrc} />
-        <div className="overlay">
-          <h4 className="product_name">{this.props.name}</h4>
-        </div>
-        <div className="fa fa-check-circle"></div>
-      </div>
-    );
-  }
-}
-
 export default ProductListItem;
